31b797b31aab7f9aa4116ea0d43170cce571d18d
Muhammad Okasha <okashakhan461@gmail.com>
2023-03-30 01:09:11 +0500

    First Project Added

---
+++ app/src/main/java/com/example/muhammadokasha20_arid_515splashlabtask/NewRecord.java
@@ -0,0 +1,25 @@
+package com.example.muhammadokasha20_arid_515splashlabtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class NewRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_record);
+        Button btn=(Button) findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(NewRecord.this,ShowRecord.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/muhammadokasha20_arid_515splashlabtask/ShowRecord.java
@@ -0,0 +1,14 @@
+package com.example.muhammadokasha20_arid_515splashlabtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_record);
+    }
+}
